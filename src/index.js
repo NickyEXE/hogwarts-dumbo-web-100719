554229f0a11cd1521d1d89267d7ff8e2e22ebcea
NickyEXE <dovern42@gmail.com>
2019-12-13 09:36:02 -0500

    initial version plus console.hog

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import App from './components/App';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 
+console.hog = (arg1, arg2) => {
+    console.log("🐷🐷🐷🐷🐷")
+    console.log(arg1, arg2)
+    console.log("🐷🐷🐷🐷🐷")
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
